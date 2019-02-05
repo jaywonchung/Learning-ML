b75fd5dddef8498843f56607dbcd665d265f580b
jaywonchung <jaywonchung@snu.ac.kr>
2019-02-05 22:28:53 +0900

    Add denoising autoencoder feature

--- Implementations/Denoising Autoencoder/train.py
+++ Implementations/Denoising Autoencoder/train.py
@@ -17,13 +17,42 @@ from train_args import get_args
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def CE_criterion(x, y):
+	"""
+	Cross-Entropy loss function.
+	
+	Args:
+		x: output of autoencoder
+		y: input data
+	"""
 	x = torch.clamp(x, 1e-8, 1-1e-8)
 	return -torch.mean(y * torch.log(x) + (1.-y) * torch.log(1.-x))
 	
 def MSE_criterion(x, y):
+	"""
+	Mean Square Error loss function.
+	
+	Args:
+		x: output of autoencoder
+		y: input data
+	"""
 	return torch.mean((x-y)**2)
 
 def main(args):
+	"""
+	Main function that trains the model.
+	1. Retrieve arguments from args
+	2. Prepare MNIST
+	3. Train
+	4. Display first batch of test set
+	
+	Args:
+		args.add_noise: Whether to add noise (DAE) to input image or not (AE).not
+		args.epochs: How many epochs to train model.
+		args.loss: Which loss function to use. Either cross-entropy or mean square error.
+		args.lr: Learning rate.
+		args.latent_dim: Dimension of latent variable.
+		args.print_every: How often to print training progress.
+	"""
 	# Retrieve arguments
 	add_noise = args.add_noise
 	epochs = args.epochs
@@ -56,8 +85,11 @@ def main(args):
 		for batch_ind, (input_data, _) in enumerate(train_loader):
 			input_data = input_data.to(device)
 			
-			# Forward propagation
-			output = autoencoder(input_data)
+			# Forward propagation with noise added
+			if add_noise:
+				output = autoencoder(F.dropout(input_data, p=0.5))
+			else:
+				output = autoencoder(input_data)
 			
 			# Calculate loss
 			loss = criterion(output, input_data)
@@ -81,9 +113,17 @@ def main(args):
 	with torch.no_grad():
 		images, _ = iter(test_loader).next()
 		images = images.to(device)
-		output = autoencoder(images)
-		display_batch(images)
-		display_batch(output)
+		
+		if add_noise:
+			noise_images = F.dropout(images, p=0.5)
+			output = autoencoder(noise_images)
+			display_batch(images)
+			display_batch(noise_images)
+			display_batch(output)
+		else:
+			output = autoencoder(images)
+			display_batch(images)
+			display_batch(output)
 
 if __name__ == "__main__":
 	args = get_args()
