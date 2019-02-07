65343ea2a772165a73bfa21e57ccd30f694cad36
jaywonchung <jaywonchung@snu.ac.kr>
2019-02-07 17:54:25 +0900

    Remove binarize code

--- Implementations/Variational-Autoencoder/plot_utils.py
+++ Implementations/Variational-Autoencoder/plot_utils.py
@@ -3,12 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def display_batch(title, batch, data, save):
-    """Display batch of image using plt"""
-    if binarize:
-        batch[batch>=0.5] = 1.
-        batch[batch<0.5] = 0.
+    """Display and save batch of image using plt"""
     im = torchvision.utils.make_grid(batch)
-    
     plt.title(title)
     plt.imshow(np.transpose(im.cpu().numpy(), (1, 2, 0)), cmap='gray')
     plt.show()
