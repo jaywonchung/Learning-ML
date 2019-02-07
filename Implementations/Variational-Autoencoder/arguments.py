5b4ab6c74a31c70197ae744ac0589a529de3045c
jaywonchung <jaywonchung@snu.ac.kr>
2019-02-07 12:41:23 +0900

    Fix type

--- Implementations/Variational-Autoencoder/arguments.py
+++ Implementations/Variational-Autoencoder/arguments.py
@@ -1,63 +1,63 @@
 import argparse
 
 defaults = {
-	"dataset": True,
-	"decoder_type": 'Gaussian',
-	"model_sigma": False,
-	"epochs": 10,
-	"batch_size": 64,
-	"learning_rate": 3e-4,
-	"latent_dim": 10,
-	"print_every": 1
+    "dataset": 'MNIST',
+    "decoder_type": 'Gaussian',
+    "model_sigma": False,
+    "epochs": 10,
+    "batch_size": 64,
+    "learning_rate": 3e-4,
+    "latent_dim": 10,
+    "print_every": 1
 }
 
 def check_args(args):
-	"""Check commandline argument validity."""
-	assert args.dataset=='MNIST' or args.dataset=='CIFAR10', "Dataset must be either 'MNIST' for 'CIFAR10"
+    """Check commandline argument validity."""
+    assert args.dataset=='MNIST' or args.dataset=='CIFAR10', "Dataset must be either 'MNIST' for 'CIFAR10"
 
-	assert args.decoder_type=='Gaussian' or args.decoder_type=='Bernoulli', "Decoder type must be either 'Gaussian' for 'Bernoulli"
+    assert args.decoder_type=='Gaussian' or args.decoder_type=='Bernoulli', "Decoder type must be either 'Gaussian' for 'Bernoulli"
 
-	assert args.model_sigma==True or args.model_sigma==False, "Model sigma must be either True or False"
-	
-	assert args.epochs >= 1, "Number of epochs must be a positive integer"
+    assert args.model_sigma==True or args.model_sigma==False, "Model sigma must be either True or False"
+    
+    assert args.epochs >= 1, "Number of epochs must be a positive integer"
 
-	assert args.batch_size >= 1, "Size of batch must be a positive integer"
-	
-	assert args.learning_rate > 0, "Learning rate must be positive"
-	
-	assert args.latent_dim >= 1, "Latent dimension must be a positive integer"
-	
-	assert args.print_every >= 1, "print_every must be a positive integer"
-	
-	return args
+    assert args.batch_size >= 1, "Size of batch must be a positive integer"
+    
+    assert args.learning_rate > 0, "Learning rate must be positive"
+    
+    assert args.latent_dim >= 1, "Latent dimension must be a positive integer"
+    
+    assert args.print_every >= 1, "print_every must be a positive integer"
+    
+    return args
 
 def get_args():
-	"""Parse arguments from commandline."""
-	parser = argparse.ArgumentParser(
-		description="Pytorch Implementation of Denoising Autoencoder(DAE)")
-	
-	parser.add_argument("-d", "--dataset",
-		type=str, default=defaults['dataset'], help="'MNIST' or 'CIFAR10'")
+    """Parse arguments from commandline."""
+    parser = argparse.ArgumentParser(
+        description="Pytorch Implementation of Denoising Autoencoder(DAE)")
+    
+    parser.add_argument("-d", "--dataset",
+        type=str, default=defaults['dataset'], help="'MNIST' or 'CIFAR10'")
 
-	parser.add_argument("-t", "--decoder_type",
-		type=str, default=defaults['decoder_type'], help="'Gaussian' or 'Bernoulli'")
+    parser.add_argument("-t", "--decoder_type",
+        type=str, default=defaults['decoder_type'], help="'Gaussian' or 'Bernoulli'")
 
-	parser.add_argument("-s", "--model_sigma",
-		type=bool, default=defaults['model_sigma'], help="In case of Gaussian decoder, whether to model the standard deviation")
-	
-	parser.add_argument("-e", "--epochs",
-		type=int, default=defaults['epochs'], help="Number of epochs to train")
+    parser.add_argument("-s", "--model_sigma",
+        type=bool, default=defaults['model_sigma'], help="In case of Gaussian decoder, whether to model the standard deviation")
+    
+    parser.add_argument("-e", "--epochs",
+        type=int, default=defaults['epochs'], help="Number of epochs to train")
 
-	parser.add_argument("-b", "--batch_size",
-		type=int, default=defaults['batch_size'], help="Size of batch at training/testing")
-	
-	parser.add_argument("-lr", "--learning_rate",
-		type=float, default=defaults['learning_rate'], help="Learning rate for adam optimizer")
-	
-	parser.add_argument("-z", "--latent_dim",
-		type=int, default=defaults['latent_dim'], help="Dimension of latent variable z")
-	
-	parser.add_argument("-p", "--print_every",
-		type=int, default=defaults['print_every'], help="How often to print loss progress")
-	
-	return check_args(parser.parse_args())
+    parser.add_argument("-b", "--batch_size",
+        type=int, default=defaults['batch_size'], help="Size of batch at training/testing")
+    
+    parser.add_argument("-lr", "--learning_rate",
+        type=float, default=defaults['learning_rate'], help="Learning rate for adam optimizer")
+    
+    parser.add_argument("-z", "--latent_dim",
+        type=int, default=defaults['latent_dim'], help="Dimension of latent variable z")
+    
+    parser.add_argument("-p", "--print_every",
+        type=int, default=defaults['print_every'], help="How often to print loss progress")
+    
+    return check_args(parser.parse_args())
--- Implementations/Variational-Autoencoder/model.py
+++ Implementations/Variational-Autoencoder/model.py
@@ -5,9 +5,9 @@ import torch.nn.functional as F
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class GaussianEncoder(nn.Module):
-	"""Gaussian encoder module for VAE"""
+    """Gaussian encoder module for VAE"""
 
-	def __init__(self, latent_dim, dataset):
+    def __init__(self, latent_dim, dataset):
         """
         Constructor for the GaussianEncoder class
 
@@ -15,7 +15,7 @@ class GaussianEncoder(nn.Module):
             latent_dim: Dimension of the latent variable
             dataset: Type of dataset to use. Either 'MNIST' or 'CIFAR10'
         """
-		super().__init__()
+        super().__init__()
         self.latent_dim = latent_dim
         self.dataset = dataset
 
@@ -32,9 +32,9 @@ class GaussianEncoder(nn.Module):
             self.fc4 = nn.Linear(in_features=1024, out_features=2*latent_dim)
             # x: (N, 2*latent_dim)
 
-        elif dataset = 'CIFAR10':
+        elif dataset == 'CIFAR10':
             # x: (N, 3, 32, 32)
-		    self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=4, stride=2, padding=1)
+            self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=4, stride=2, padding=1)
             # x: (N, 64, 16, 16)
             self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1)
             self.bn2 = nn.BatchNorm2d(num_features=64)
@@ -45,12 +45,12 @@ class GaussianEncoder(nn.Module):
             self.fc4 = nn.Linear(in_features=256*4*4, out_features=2*latent_dim)
             # x: (N, 2*latent_dim)
 
-		for m in self.modules():
-			if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
-				nn.init.kaiming_normal_(m.weight)
-				m.bias.data.fill_(0.)
+        for m in self.modules():
+            if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+                m.bias.data.fill_(0.)
 
-	def forward(self, x):
+    def forward(self, x):
         """
         Forward method for the GaussianEncoder class
 
@@ -86,9 +86,9 @@ class GaussianEncoder(nn.Module):
 
 
 class BernoulliDecoder(nn.Module):
-	"""BernoulliDecoder module for VAE with MNIST dataset"""
+    """BernoulliDecoder module for VAE with MNIST dataset"""
 
-	def __init__(self, latent_dim):
+    def __init__(self, latent_dim):
         """
         Constructor for the BernoulliDecoder class
         Can only model black and white MNIST images
@@ -96,7 +96,7 @@ class BernoulliDecoder(nn.Module):
         Parameter:
             latent_dim: Dimension of the latent variable
         """
-		super().__init__()
+        super().__init__()
 
         # z: (N, latent_dim)
         self.fc1 = nn.Linear(in_features=latent_dim, out_features=1024)
@@ -110,13 +110,13 @@ class BernoulliDecoder(nn.Module):
         # z: (N, 64, 14, 14)
         self.deconv4 = nn.ConvTranspose2d(in_channels=64, out_channels=1, kernel_size=4, stride=2, padding=1)
         # z: (N, 1, 28, 28)
-		
-		for m in self.modules():
+        
+        for m in self.modules():
             if isinstance(m, nn.Linear) or isinstance(m, nn.ConvTranspose2d):
-				nn.init.kaiming_normal_(m.weight)
-				m.bias.data.fill_(0.)
-	
-	def forward(self, z):
+                nn.init.kaiming_normal_(m.weight)
+                m.bias.data.fill_(0.)
+    
+    def forward(self, z):
         """
         Forward method for the BernoulliDecoder class
 
@@ -129,15 +129,15 @@ class BernoulliDecoder(nn.Module):
             sigma: Vector of size latent_dim.
                    Each element represents the standard deviation of a Gaussian distribution.
         """
-		z = F.relu(self.bn1(self.fc1(z)))
+        z = F.relu(self.bn1(self.fc1(z)))
         z = F.relu(self.bn2(self.fc2(z)))
         z = z.view(-1, 128, 7, 7)
         z = F.relu(self.bn3(self.deconv3(z)))
         z = F.sigmoid(self.deconv4(z))
-		return z
+        return z,
 
 
-class GaussianDeccoder(nn.Module):
+class GaussianDecoder(nn.Module):
     """GaussianDecoder module for VAE"""
 
     def __init__(self, latent_dim, dataset, model_sigma=False):
@@ -213,38 +213,38 @@ class GaussianDeccoder(nn.Module):
         if self.dataset == 'MNIST':
             z = F.relu(self.bn1(self.fc1(z)))
             z = F.relu(self.bn2(self.fc2(z)))
-            z = z.view(-1, 127, 7, 7)
+            z = z.view(-1, 128, 7, 7)
             z = F.relu(self.bn3(self.deconv3(z)))
-            if model_sigma:
+            if self.model_sigma:
                 gaussian = self.deconv4(z)
                 mu = F.sigmoid(gaussian[:, 0, :, :])
                 sigma = 1e-6 + F.softplus(gaussian[:, 1, :, :])
                 return mu, sigma
             else:
                 mu = F.sigmoid(self.deconv4(z))
-                return mu
+                return mu,
         
         elif self.dataset == 'CIFAR10':
             z = F.relu(self.bn1(self.fc1(z)))
             z = F.relu(self.bn2(self.deconv2(z)))
             z = F.relu(self.deconv3(z))
             z = F.relu(self.deconv4(z))
-            if model_sigma:
+            if self.model_sigma:
                 gaussian = self.deconv5(z)
                 mu = F.sigmoid(gaussian[:, :3, :, :])
                 sigma = 1e-6 + F.softplus(gaussian[:, 3:, :, :])
                 return mu, sigma
             else:
                 mu = F.sigmoid(self.deconv5(z))
-                return mu
+                return mu,
 
 
 
 
 class VAE(nn.Module):
-	"""Variational Autoencoder module that wraps one encoder and one decoder module."""
-	
-	def __init__(self, latent_dim, dataset, decoder_type, model_sigma=False):
+    """Variational Autoencoder module that wraps one encoder and one decoder module."""
+    
+    def __init__(self, latent_dim, dataset, decoder_type, model_sigma=False):
         """
         Constructor for VAE class
 
@@ -255,14 +255,14 @@ class VAE(nn.Module):
             model_sigma: Whether to model standard deviations too.
                          If True, forward method returns (mu, sigma). Else, returns only mu.
         """
-		super().__init__()
+        super().__init__()
         self.encoder = GaussianEncoder(latent_dim, dataset)
         if decoder_type == 'Bernoulli':
             self.decoder = BernoulliDecoder(latent_dim)
-        elif decoder_tpye == 'Gaussian':
+        elif decoder_type == 'Gaussian':
             self.decoder = GaussianDecoder(latent_dim, dataset, model_sigma)
-	
-	def forward(self, x):
+    
+    def forward(self, x):
         """
         Forward method for the GaussianEncoder class
         Samples latent variable z from the distribution calculated by the encoder,
