0dea4c565f25b9a81a4942c09d898b8f8ad19d71
jaywonchung <jaywonchung@snu.ac.kr>
2019-02-09 00:08:44 +0900

    Change number of rows dynamically

--- Implementations/Variational-Autoencoder/plot_utils.py
+++ Implementations/Variational-Autoencoder/plot_utils.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 def display_and_save_batch(title, batch, data, save=True, display=True):
     """Display and save batch of image using plt"""
-    im = torchvision.utils.make_grid(batch)
+    im = torchvision.utils.make_grid(batch, nrow=int(batch.shape[0]**0.5))
     plt.title(title)
     plt.imshow(np.transpose(im.cpu().numpy(), (1, 2, 0)), cmap='gray')
     if save:
