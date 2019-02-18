180fa3d35825a8134221427616218574d11105bf
jaywonchung <jaywonchung@snu.ac.kr>
2019-02-18 15:11:13 +0900

    Add function that displays and saves batch oflatent variables

--- Implementations/Conditional-Variational-Autoencoder/plot_utils.py
+++ Implementations/Conditional-Variational-Autoencoder/plot_utils.py
@@ -8,6 +8,16 @@ def display_and_save_batch(title, batch, data, save=True, display=True):
     plt.title(title)
     plt.imshow(np.transpose(im.cpu().numpy(), (1, 2, 0)), cmap='gray')
     if save:
-        plt.savefig('results/'+title+data+'.png', transparent=True, bbox_inches='tight')
+        plt.savefig('results/' + title + data + '.png', transparent=True, bbox_inches='tight')
+    if display:
+        plt.show()
+
+def display_and_save_latent(batch, data, save=True, display=True):
+    """Display and save batch of 2-D latent variable"""
+    plt.title('Latent variables')
+    z = batch.cpu().numpy()
+    plt.plot(z[:,0], z[:,1], 'o', color='black')
+    if save:
+        plt.savefig('results/latent-variable-' + data + '.png', transparent=True, bbox_inches='tight')
     if display:
         plt.show()
