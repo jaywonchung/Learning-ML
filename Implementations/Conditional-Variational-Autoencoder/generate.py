c3d7ddd1982079c425a21cd15938d227e05b66b4
jaywonchung <jaywonchung@snu.ac.kr>
2019-02-18 14:07:46 +0900

    Remove CIFAR10 support

--- Implementations/Conditional-Variational-Autoencoder/generate.py
+++ Implementations/Conditional-Variational-Autoencoder/generate.py
@@ -8,15 +8,13 @@ from plot_utils import display_and_save_batch
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def generate_images(mode='uniform', dataset='MNIST', dim=0, num=400, grid_size=0.05, PATH=None, model=None):
+def generate_images(mode='uniform', num=400, grid_size=0.05, PATH=None, model=None):
     """
     Generates imgaes with 2D latent variables sampled uniformly with mean 0
     Currently supports Bernoulli decoders and Gaussian decoders without sigmas
 
     Args:
         mode: 'uniform' or 'random'
-        dataset:' MNIST' or 'CIFAR10', in accordance with the model
-        dim: we change dim and dim+1 dimensions of the latent variable (CIFAR10 only)
         num: Number of samples to make. Accepts square numbers
         grid_size: Distance between adjacent latent variables
         PATH: The path to saved model (saved with torch.save(model, path))
@@ -29,8 +27,6 @@ def generate_images(mode='uniform', dataset='MNIST', dim=0, num=400, grid_size=0
     # Check arguments
     if mode!='uniform' and mode!='random':
         raise ValueError("Argument mode should either be 'uniform' or 'random'")
-    if dataset!='MNIST' and dataset!='CIFAR10':
-        raise ValueError("Argument datset should either be 'MNIST' or 'CIFAR10")
     if num!=(int(num**0.5))**2:
         raise ValueError('Argument num should be a square number')
     if PATH and model:
@@ -48,16 +44,9 @@ def generate_images(mode='uniform', dataset='MNIST', dim=0, num=400, grid_size=0
         axis = (torch.arange(side) - side//2) * grid_size
         x = axis.reshape(1, -1)
         y = x.transpose(0, 1)
-        _z = torch.stack(torch.broadcast_tensors(x, y), 2).reshape(-1, 2).to(device)
+        z = torch.stack(torch.broadcast_tensors(x, y), 2).reshape(-1, 2).to(device)
     elif mode == 'random':
-        _z = torch.randn((num, 2), device=device)
-    
-    # Pad latent vector with random normal numbers for CIFAR10
-    if dataset == 'CIFAR10':
-        z = torch.randn(model.latent_dim, device=device).repeat(num, 1)
-        z[:, dim:dim+2] = _z
-    else:
-        z = _z
+        z = torch.randn((num, 2), device=device)
 
     # Generate output from decoder
     with torch.no_grad():
@@ -70,4 +59,4 @@ def generate_images(mode='uniform', dataset='MNIST', dim=0, num=400, grid_size=0
     
 if __name__=="__main__":
     # commandline input
-    generate_images(mode=sys.argv[1], dataset=sys.argv[2], PATH=sys.argv[3])
+    generate_images(mode=sys.argv[1], PATH=sys.argv[2])
