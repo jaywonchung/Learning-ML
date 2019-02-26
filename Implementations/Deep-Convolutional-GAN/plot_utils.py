a6c27d1e8770db49c56a62f9e1cef3e36db2f56a
jaywonchung <jaywonchung@snu.ac.kr>
2019-02-26 22:14:44 +0900

    Refine display_batch function

--- Implementations/Deep-Convolutional-GAN/plot_utils.py
+++ Implementations/Deep-Convolutional-GAN/plot_utils.py
@@ -3,14 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def display_batch(title, batch):
+def display_batch(batch, title, save=True):
     """Display batch of image using plt"""
-    im = torchvision.utils.make_grid(batch, nrow=int(batch.shape[0]**0.5))
+    im = torchvision.utils.make_grid(batch, nrow=int(batch.shape[0]**0.5), padding=2, normalize=True)
     plt.title(title)
-    plt.imshow(np.transpose(im.cpu().numpy(), (1, 2, 0)))
+    plt.imshow(np.transpose(im.cpu().detach().numpy(), (1, 2, 0)))
+    if save:
+        plt.savefig('results/' + title + '.png', transparent=True, bbox_inches='tight')
     plt.show()
 
-def display_and_save_latent(batch, label, data, save=True, display=True):
+
+def display_and_save_latent(batch, label, data, isplay=True):
     """Display and save batch of 2-D latent variable using plt"""
     colors = ['black', 'red', 'green', 'blue', 'yellow', 'cyan', 'magenta', 'pink', 'violet', 'grey']
     z = batch.cpu().detach().numpy()
@@ -22,5 +25,4 @@ def display_and_save_latent(batch, label, data, save=True, display=True):
     plt.ylim(-3, 3)
     if save:
         plt.savefig('results/latent-variable' + data + '.png', transparent=True, bbox_inches='tight')
-    if display:
-        plt.show()
+    plt.show()
